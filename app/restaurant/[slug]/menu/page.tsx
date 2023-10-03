867207a0c214b6ec2e239ac8d1dc3246491b4e36
Fernando Goncalves <jimmynando@gmail.com>
2023-10-03 15:38:59 -0300

    using layout on nested components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,18 +1,12 @@
 import RestaurantNavBar from "../components/RestaurantNavBar";
-import Header from "../components/Header";
 import Menu from "../components/Menu";
 
 const RestaurantMenu = () => {
   return (
-    <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[100%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Menu />
-        </div>
-      </div>
-    </>
+    <div className="bg-white w-[100%] rounded p-3 shadow">
+      <RestaurantNavBar />
+      <Menu />
+    </div>
   );
 };
 
