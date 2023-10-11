eb13b131657db43080a6b2f7c6659ee23a20322c
Fernando Goncalves <jimmynando@gmail.com>
2023-10-11 15:28:38 -0300

    adding the modal content

--- app/components/LoginModal.tsx
+++ app/components/LoginModal.tsx
@@ -11,9 +11,8 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 600,
   bgcolor: "background.paper",
-  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
@@ -45,12 +44,21 @@ export default function LoginModal({ isSignIn }: { isSignIn: boolean }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <div className="p-2">
+            <div className="uppercase font-bold text-center pb-2 border-b mb-2">
+              <p className="text-small">
+                {renderContent("Sign in", "Create Account")}
+              </p>
+            </div>
+            <div className="w-100 m-auto">
+              <h2 className="text-2xl font-light text-center">
+                {renderContent(
+                  "Log Into Your Account",
+                  "Create Your OpenTable Account"
+                )}
+              </h2>
+            </div>
+          </div>
         </Box>
       </Modal>
     </div>
